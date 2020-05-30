5ace4b5af6792a4affccbdc3571e09a2516bc246
Vasilis Kortsimelidis <vaskort@gmail.com>
2020-05-30 10:42:18 +0100

    Update code and add comments

--- src/App.js
+++ src/App.js
@@ -6,63 +6,11 @@ import "./App.css";
 export default function App() {
   const [modalOpen, setModalOpen] = useState(false);
 
-  const trapFocus = useCallback((element, prevFocusableElement) => {
-    console.log("element eisai", element);
-    const focusableEls = Array.from(
-      element.querySelectorAll(
-        'a[href]:not([disabled]), button:not([disabled]), textarea:not([disabled]), input[type="text"]:not([disabled]), input[type="radio"]:not([disabled]), input[type="checkbox"]:not([disabled]), select:not([disabled]), .modal__close'
-      )
-    );
-    const firstFocusableEl = focusableEls[0];
-    const lastFocusableEl = focusableEls[focusableEls.length - 1];
-    let currentFocus = null;
-
-    firstFocusableEl.focus();
-    currentFocus = firstFocusableEl;
-
-    const onKeyDown = e => {
-      console.log(e.target);
-      e.preventDefault();
-      if (focusableEls.includes(e.target)) {
-        currentFocus = e.target;
-      } else {
-        // if you're on the first focusable element and using the shift key
-        if (currentFocus === firstFocusableEl) {
-          lastFocusableEl.focus();
-        } else {
-          // you're on the last so go to the first one
-          firstFocusableEl.focus();
-        }
-        currentFocus = document.activeElement;
-      }
-    };
-
-    document.addEventListener("focus", onKeyDown, true);
-
-    return {
-      onClose: () => {
-        console.log("clear up events");
-        document.removeEventListener("focus", onKeyDown);
-        prevFocusableElement.focus();
-      }
-    };
-  }, []);
-
-  useEffect(() => {
-    if (!modalOpen) return;
-    
-    const removeListener = trapFocus(<Modal/>);
-
-    return () => {
-      removeListener.onClose();
-    };
-  },[modalOpen, trapFocus]);
-
   return (
     <div className="App">
       <h1>Trap Focus Function Example</h1>
 
-      <button onClick={() => setModalOpen(!modalOpen)}>Open modal</button>
+      <button onClick={() => setModalOpen(!modalOpen)} type="button" tabIndex="0">Open modal</button>
       <Modal visible={modalOpen} onClose={() => setModalOpen(!modalOpen)} />
     </div>
   );
--- src/Modal/Modal.js
+++ src/Modal/Modal.js
@@ -1,17 +1,73 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useCallback, useEffect, useRef } from "react";
 import styles from "./Modal.module.css";
 
 const Modal = ({ visible, onClose }) => {
+  const modal = useRef(null);
+
+  const trapFocus = useCallback((element, prevFocusableElement = document.activeElement) => {
+    const focusableEls = Array.from(
+      element.current.querySelectorAll(
+        'a[href]:not([disabled]), button:not([disabled]), textarea:not([disabled]), input[type="text"]:not([disabled]), input[type="radio"]:not([disabled]), input[type="checkbox"]:not([disabled]), select:not([disabled])'
+      )
+    );
+    const firstFocusableEl = focusableEls[0];
+    const lastFocusableEl = focusableEls[focusableEls.length - 1];
+    let currentFocus = null;
+
+    firstFocusableEl.focus();
+    currentFocus = firstFocusableEl;
+
+    const onKeyDown = e => {
+      e.preventDefault();
+      // if the focused element "lives" in your modal container then just focus it
+      if (focusableEls.includes(e.target)) {
+        currentFocus = e.target;
+      } else {
+        // you're out of the container
+        // if previously the focused element was the first element then focus the last 
+        // element - means you were using the shift key
+        if (currentFocus === firstFocusableEl) {
+          lastFocusableEl.focus();
+        } else {
+          // you previously were focused on the last element so just focus the first one
+          firstFocusableEl.focus();
+        }
+        // update the current focus var
+        currentFocus = document.activeElement;
+      }
+    };
+
+    document.addEventListener("focus", onKeyDown, true);
+
+    return {
+      onClose: () => {
+        document.removeEventListener("focus", onKeyDown);
+        prevFocusableElement.focus();
+      }
+    };
+  }, []);
+
+  useEffect(() => {
+    if (!visible) return;
+    
+    const trapped = trapFocus(modal);
+
+    return () => {
+      trapped.onClose();
+    };
+  },[visible, trapFocus]);
 
   return (
     visible && (
       <Fragment>
+        <div tabIndex="0"></div>
         <div className={styles.overlay} />
         <div
           role="dialog"
           aria-labelledby="modalTitle"
           aria-modal="true"
           className={styles.modal}
+          ref={modal}
         >
           <h2 id="modalTitle">Free Gift Form</h2>
           <form>
@@ -40,6 +96,7 @@ const Modal = ({ visible, onClose }) => {
           </form>
           <button onClick={onClose}>Close Modal</button>
         </div>
+        <div tabIndex="0"></div>
       </Fragment>
     )
   );
